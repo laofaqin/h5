914b14eb63356da5c1d61b17d58cb752772665a8
mooncup <18438613138@163.com>
2020-04-22 19:23:40 +0800

    ajd

---
+++ 资产总览/js/api.js
@@ -0,0 +1,7 @@
+var api = {
+    login: 'user/login',//登录
+    walletInfo: 'userwallet/info',
+    shengji:"log/upgradeIntegralLog",
+    dayIncome:"log/dayIncome",
+    incomelist:"log/dayIncomeDetail",
+};
--- 资产总览/js/request.js
+++ 资产总览/js/request.js
@@ -1,5 +1,62 @@
+document.write("<script language=javascript src='/js/api.js'></script>");
 document.write("<script language=javascript src='/js/axios.js'></script>");
 
+
+var typename = [
+    { "type": "zichan", "name": "资产总记录" },
+    { "type": "shengji", "name": "升级积分", number: 3 },
+    { "type": "xianjin", "name": "现金消费", number: 1 },
+    { "type": "tuijian", "name": "推荐业绩积分", number: 4 },
+    { "type": "jifen", "name": "积分消费", number: 5 },
+    { "type": "fenhong", "name": "分红积分", number: 6 },
+    { "type": "gongxiang", "name": "共享奖励", number: 7 },
+    { "type": "shangcheng", "name": "商城收益", number: 2 },
+    { "type": "xianshang", "name": "线上收益", number: 11 },
+    { "type": "huokuan", "name": "货款收入", number: 8 }
+];
+
+function goback() {
+    window.history.back();
+};
+
+const formatNumber = n => {
+    n = n.toString()
+    return n[1] ? n : '0' + n
+}
+//格式化时间
+function formatTimeTwo(number, format) {
+
+    var formateArr = ['Y', 'M', 'D', 'h', 'm', 's'];
+    var returnArr = [];
+
+    var date = new Date(number);
+    returnArr.push(date.getFullYear());
+    returnArr.push(formatNumber(date.getMonth() + 1));
+    returnArr.push(formatNumber(date.getDate()));
+
+    returnArr.push(formatNumber(date.getHours()));
+    returnArr.push(formatNumber(date.getMinutes()));
+    returnArr.push(formatNumber(date.getSeconds()));
+
+    for (var i in returnArr) {
+        format = format.replace(formateArr[i], returnArr[i]);
+    }
+    return format;
+}
+
+
+function getParams() {
+    var url = location.search; //获取url中"?"符后的字串  
+    var theRequest = new Object();
+    if (url.indexOf("?") != -1) {
+        var str = url.substr(1);
+        strs = str.split("&");
+        for (var i = 0; i < strs.length; i++) {
+            theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+        }
+    }
+    return theRequest;
+}
 function setToken(cvalue) {
     document.cookie = "token=" + cvalue
 };
@@ -21,10 +78,7 @@ var url = '',
 url = productEnvironment ? productApiUrl : testApiUrl;
 
 
-var api = {
-    login: 'user/login',//登录
 
-};
 
 
 var Request = function (method, api, params, success, fail) {
@@ -32,7 +86,7 @@ var Request = function (method, api, params, success, fail) {
     var requestParams = {
         method: method,
         url: url + api,
-        header: {
+        headers: {
             "Authorization": token,
             "content-type": "application/json",
         },
@@ -45,7 +99,23 @@ var Request = function (method, api, params, success, fail) {
         requestParams.data = params || {}
     }
     return axios(requestParams).then(res => {
-        success && success(res)
+        let code = res.data.code;
+        if (code == 1) {
+            success && success(res.data.content)
+        } else if (code == 403) {
+            // 原缓存存在token，过期了
+            if (getToken()) {
+                // setToken("");
+                alert('登录信息已过期,请重新登录');
+                // location.href = '../register.html';
+            } else {
+                location.href = '../register.html';
+            }
+            // 原缓存没有token，即没绑定
+        } else {
+            alert(res.data.msg)
+            fail && fail(res.data)
+        }
     }).catch(err => {
         fail && fail(err)
     })
